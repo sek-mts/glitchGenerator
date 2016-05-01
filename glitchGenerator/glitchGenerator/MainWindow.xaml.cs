daf68b5ebd61880100474f5a18d27e9fdcdf772b
sek-mts <nuclearbraineater@gmail.com>
2016-05-01 21:35:35 +0200

    switch every second pixel to red

--- glitchGenerator/glitchGenerator/MainWindow.xaml.cs
+++ glitchGenerator/glitchGenerator/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using glitchGenerator.ViewModel;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
+//using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +27,31 @@ namespace glitchGenerator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+                try
+                {
+                    ChangePixels cP = new ChangePixels();
+                    Bitmap bmp = null;
+                    bmp = (Bitmap)Image.FromFile(ofd.FileName);
+                    bmp = cP.ChangeColor(bmp);
+                    //bmp.Save(@"<your path>");
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+
         }
     }
 }
---
+++ glitchGenerator/glitchGenerator/ViewModel/ChangePixels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+//using System.Windows.Media;
+
+namespace glitchGenerator.ViewModel
+{
+    public class ChangePixels
+    {
+        public Bitmap ChangeColor(Bitmap scrBitmap)
+        {
+            //You can change your new color here. Red,Green,LawnGreen any..
+            Color newColor = Color.Red;
+            Color actualColor;
+            //make an empty bitmap the same size as scrBitmap
+            Bitmap newBitmap = new Bitmap(scrBitmap.Width, scrBitmap.Height);
+            for (int i = 0; i < scrBitmap.Width; i++)
+            {
+                for (int j = 0; j < scrBitmap.Height; j++)
+                {
+                    //get the pixel from the scrBitmap image
+                    actualColor = scrBitmap.GetPixel(i, j);
+                    // > 150 because.. Images edges can be of low pixel colr. if we set all pixel color to new then there will be no smoothness left.
+                    if (actualColor.A > 150 && i % 2 == 0)
+                        newBitmap.SetPixel(i, j, newColor);
+                    else
+                        newBitmap.SetPixel(i, j, actualColor);
+                }
+            }
+            return newBitmap;
+        }
+    }
+}
